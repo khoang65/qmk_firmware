73d9a2a95ad10bb37d429fd1576fc15c2702370a
khoang65 <khoang65@gmail.com>
2022-06-23 19:45:12 -0400

    moved keyboard init back to erogodash/rev1

--- keyboards/omkbd/ergodash/rev1/keymaps/khoang65/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/khoang65/keymap.c
@@ -376,7 +376,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               set_mods(mod_state);
             } else if (isCtrlPressed) {
               del_mods(MOD_MASK_CTRL);
-              VIM_SCROLL_HALF_DOWN();
+              //VIM_SCROLL_HALF_DOWN();
               set_mods(mod_state);
             } else {
               VIM_LEADER(VIM_D);
@@ -552,7 +552,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (PRESSED) { 
         if (isCtrlPressed) {
           del_mods(MOD_MASK_CTRL); 
-          VIM_SCROLL_HALF_UP(); 
+          //VIM_SCROLL_HALF_UP(); 
           set_mods(mod_state);
         } else {
           VIM_UNDO();
@@ -609,6 +609,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return true;
   }
 } 
+
+// ************************************************ //
+// *********** KEYBOARD PRE/POST INIT ************* //
+// ************************************************ //
+void keyboard_pre_init_user(void) {
+  // rgblight_disable_noeeprom(); // ideally disables RBG on startup, prevents LED flash on plugin; needs to be moved to POST
+}
+
+// Numlock on boot <https://github.com/qmk/qmk_firmware/issues/10890#issuecomment-927222187>
+void numlock_on(void) {
+  led_t led_state = host_keyboard_led_state();
+  bool isNumlock = led_state.num_lock;
+  if (!isNumlock) {
+      register_code(KC_NUMLOCK);
+      unregister_code(KC_NUMLOCK);
+  }
+}
+
+void keyboard_post_init_user() {
+  numlock_on();
+  #ifdef RGBLIGHT_ENABLE
+  rgblight_disable_noeeprom();
+  #endif // !RGBLIGHT_ENABLE
+}
 
 // ************************************************ //
 // ******** DYNAMIC TAP HOLD CONFIGURATION ******** //
