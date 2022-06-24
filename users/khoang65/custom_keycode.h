43eec1b7ed3c017c51500462622543041f714c61
khoang65 <khoang65@gmail.com>
2022-06-24 16:10:17 -0400

    updated headers to use the SPDX identifier

--- users/khoang65/custom_keycode.h
+++ users/khoang65/custom_keycode.h
@@ -1,19 +1,5 @@
- /* Copyright 2022-2022 Kevin Hoang
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+// Copyright 2022 Kevin Hoang (@khoang65) <khoang65@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /**
  * QMK functions can't register custom keycodes, but we can setup a keyrecord_t and call process_record_kb() directly.
